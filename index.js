cad10053d66cc0e32fdaa422f23a3c1c253930dd
Tyler Alder <tyler.b.alder@gmail.com>
2018-06-04 02:54:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,11 +72,11 @@ function removeFromCart(item) {
 }
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    return (`Your total cost is $${total()}, which will be charged to ${cardNumber}.`); 
+    return (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`); 
   } else {
    return ("Sorry, we don't have a credit card on file for you.");
   }
    for (var i = cart.length; i>= 0; i--) {
-      cart.pop();
+      cart.shift();
     } 
 }
