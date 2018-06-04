65419ac2d2d71f00ffa81cc6adad956cd9a1647a
Tyler Alder <tyler.b.alder@gmail.com>
2018-06-04 02:58:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function placeOrder(cardNumber) {
     cart.pop();
   }
   } else {
-   console.log ("Sorry, we don't have a credit card on file for you.");
+   return ("Sorry, we don't have a credit card on file for you.");
   }
   
     
