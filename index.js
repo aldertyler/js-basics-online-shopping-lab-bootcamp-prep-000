fa8a2f27e6efd971ff5813244f56fa4169c2936c
Tyler Alder <tyler.b.alder@gmail.com>
2018-06-04 02:42:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ function removeFromCart(item) {
       }
 }
 function placeOrder(cardNumber) {
-  if (cardNumber {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber).`)
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   } else {
-    console.log('Sorry, we don't have a credit card on file for you.');
+    console.log("Sorry, we don't have a credit card on file for you.");
   }
 }
